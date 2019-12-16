66dfea16a3f0f9c0b0db87122892b527d001c096
thoughtstile <thoughtstile@gmail.com>
2019-12-16 08:40:39 -0800

    changed wording

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -19,7 +19,7 @@ describe('Articles Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db', db);
   });
@@ -80,15 +80,15 @@ describe('Articles Endpoints', function() {
           });
       });
 
-      // it('removes XSS attack content', () => {
-      //   return supertest(app)
-      //     .get(`/api/articles`)
-      //     .expect(200)
-      //     .expect(res => {
-      //       expect(res.body[0].title).to.eql(expectedArticle.title);
-      //       expect(res.body[0].content).to.eql(expectedArticle.content);
-      //     });
-      // });
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/api/articles`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body[0].title).to.eql(expectedArticle.title);
+            expect(res.body[0].content).to.eql(expectedArticle.content);
+          });
+      });
     });
   });
 
